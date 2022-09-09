2be1f0790d0212586b23d3542f983605fea7c4d1
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-09 20:54:59 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+ int main() 
+ { 
+         int i; 
+         char ch[100]="abcdfghijklmnoprstuvwxyz"; 
+         for(i=0;i<24;i++) 
+         { 
+                 putchar(ch[i]); 
+         } 
+         putchar('\n'); 
+         return 0; 
+ }
