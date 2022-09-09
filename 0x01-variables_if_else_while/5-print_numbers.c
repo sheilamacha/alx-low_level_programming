19c756d181f7e7b4fabd90dfb46570540929ac21
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-09 23:13:30 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,11 @@
-#include <stdio.h> 
-  
- int main() 
- { 
-      
-     int num; 
-    for (num = '0'; num <='9'; num++) 
-     { 
-              
-         putchar( num); 
-     } 
-     putchar('\n'); 
-      
-    
-  
-     return 0; 
- }
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0-9.
+(*
+ * Return: 0 on success 
+ */
+int main(void)
+{
+        printf("0123456789\n);
+        return (0);
+}
