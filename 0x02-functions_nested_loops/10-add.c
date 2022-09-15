77dcb03961ce306b74c68426002bbc05f6575202
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-16 02:28:14 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * * add(int, int) ~ function for summation
+ * int a ~variable (1)
+ * int b ~ variable (2)
+ *  main ~ check the code
+ *  Return (0)
+ *   Return (result) ~ the result is pushed to main function
+ *    **/
+
+int add(int a, int b)
+{
+           int result = a + b;
+
+               return (result);
+
+}
