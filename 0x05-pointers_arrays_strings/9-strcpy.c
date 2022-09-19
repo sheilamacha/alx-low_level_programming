cdd974f4e0c567feee75b5c9a15cbe94a7545d89
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-19 11:43:50 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+* _strcpy - Copiar el contenido de uno a orta variable
+* @dest: This is destiny
+* @src: This is the copia
+*
+* Return: This return copy
+*/
+char *_strcpy(char *dest, char *scr)
+{
+char *start = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
+}
