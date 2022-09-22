722ff3e39d5ebec67b1a329cb557d1e3a9dec354
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-22 10:56:47 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,32 +1,20 @@
 #include "main.h"
 
 /**
-* _strstr - locates a substring
-* @haystack: string in which to check for needle
-* @needle: substring to find in haystack
-*
-* Return: pointer to beginning of needle in haystack or NULL if no match
+*string_toupper - changes all lowercases letters of a string to uppercase
+*@str: parameter
+*Return: returns a character
 */
-char *_strstr(char *haystack, char *needle)
-{
-unsigned int i = 0, j = 0;
 
-while (haystack[i])
+char *string_toupper(char *str)
 {
-while(needle[j] && (haystack[i] == needle[0]))
+int index = 0;
+
+while (str[index++])
 {
-if(haystack[i + j] == needle[j])
-j++;
-else
-break;
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
 }
-if (needle[j])
-{
-i++;
-j = 0;
-}
-else
-return (haystack + i);
-}
-return (0);
+
+return (str);
 }
