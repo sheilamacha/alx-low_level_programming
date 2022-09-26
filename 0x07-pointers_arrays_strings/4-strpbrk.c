1393d5f3a77ce4abc304d47fb2175598d687ae90
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-26 18:55:01 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,12 +12,12 @@ int i, n;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-for (n = 0; accept[n] != '\0' ; n++)
+for (n = 0; accept[n] != '\0'; n++)
 {
 if (s[i] == accept [n])
 return (s + i);
 }
 }
 
-return (NULL);
+return (0);
 }
