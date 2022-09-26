efcf899f000b25a03514b41ecb57aa319b2969e6
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-09-26 18:44:28 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strpbrk - prints the consecutive characters of s1 that are in s2.
+* @s: source string
+* @accept: searching string
+*
+* Return: new string.
+*/
+char *strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
 
+for (i = 0; *(accept + j); j++)
+{
+if (*(s + i) == *(accept + j))
+{
+break;
+}
+}
+if (*(accept + j) != '\0')
+{
+return (s + i);
+}
+}
+return (0);
+}
