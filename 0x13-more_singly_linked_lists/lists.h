ddeb4b6c190ed09c666e4cebde557e69e004a485
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-10-29 00:17:51 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,8 +6,7 @@
   * @n: integer 
   * @next: points to the next node 
   * 
-  * Description: singly linked list node structure 
-  * for Holberton project 
+  * Description: singly linked list node structure
   */ 
  typedef struct listint_s 
  { 
