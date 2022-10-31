4f647f5a33cbcc890d5d55a7c68dfc4f024e47b2
sheilamacha <111003634+sheilamacha@users.noreply.github.com>
2022-10-31 18:09:32 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,15 @@
 */
 unsigned int _pow(unsigned int num, unsigned int n)
 {
-        unsigned int sum, i;
+unsigned int sum, i;
 
-        sum = 1;
-        for (i = 0; i < n; i++)
-        {
-                sum = sum * num;
+sum = 1;
+for (i = 0; i < n; i++)
+{
+sum = sum * num;
 
-        }
-        return (sum);
+}
+return (sum);
 }
 
 /**
@@ -28,30 +28,30 @@ unsigned int _pow(unsigned int num, unsigned int n)
 unsigned int binary_to_uint(const char *b)
 {
 
-        unsigned int decimal, i, k;
+unsigned int decimal, i, k;
 
-        decimal = 0;
-        i = 0;
-        decimal = 0;
-        if (b == NULL)
-                return (0);
-        for (k = 0; b[k] != '\0'; k++)
-        {
-                if (((b[k]) != '0') && ((b[k]) ! = '1') && ((b[k]) != '\0'))
-                        return (0);
-        }
-        k = k - 1;
-        while (b[i] != '\0')
-        {
-                if ((b[k - i]) == '1')
-                {
-                        if (i == 0)
-                                decimal += 1;
-                        else
-                                decimal += _pow(2, i);
-                }
-                i++;
-         }
-         return (decimal);
+decimal = 0;
+i = 0;
+decimal = 0;
+if (b == NULL)
+return (0);
+for (k = 0; b[k] != '\0'; k++)
+{
+if (((b[k]) != '0') && ((b[k]) ! = '1') && ((b[k]) != '\0'))
+return (0);
+}
+k = k - 1;
+while (b[i] != '\0')
+{
+if ((b[k - i]) == '1')
+{
+if (i == 0)
+decimal += 1;
+else
+decimal += _pow(2, i);
+}
+i++;
+}
+return (decimal);
 }
            
